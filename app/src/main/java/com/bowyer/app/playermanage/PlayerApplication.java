f9d5dfa724b62b7eb7cbd3669d80eefd17a2632e
bowyer-app <bowyer.app@gmail.com>
2016-05-14 10:32:19 +0900

    multidex

--- app/src/main/java/com/bowyer/app/playermanage/PlayerApplication.java
+++ app/src/main/java/com/bowyer/app/playermanage/PlayerApplication.java
@@ -2,6 +2,7 @@ package com.bowyer.app.playermanage;
 
 import android.app.Application;
 import android.content.Context;
+import android.support.multidex.MultiDex;
 import com.bowyer.app.playermanage.logic.GrowthbeatLogic;
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
@@ -21,5 +22,10 @@ public class PlayerApplication extends Application {
 
   public static PlayerAppComponent getComponent(Context context) {
     return ((PlayerApplication) context.getApplicationContext()).playerAppComponent;
+  }
+
+  @Override protected void attachBaseContext(Context base) {
+    super.attachBaseContext(base);
+    MultiDex.install(this);
   }
 }
