3d3236de515ed0493959dde4280f48ea5f7d55e2
bowyer <bowyer.app@gmail.com>
2021-04-26 17:34:32 +0900

    Remove ButterKnife

--- app/src/main/java/com/bowyer/app/playermanage/ui/activity/MainActivity.java
+++ app/src/main/java/com/bowyer/app/playermanage/ui/activity/MainActivity.java
@@ -4,29 +4,22 @@ import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.RadioGroup;
-import android.widget.TextView;
-import butterknife.Bind;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 import com.bowyer.app.playermanage.PlayerApplication;
 import com.bowyer.app.playermanage.R;
 import com.bowyer.app.playermanage.database.dao.PlayerDao;
 import com.bowyer.app.playermanage.database.dto.Rank;
 import com.bowyer.app.playermanage.database.dto.Sex;
+import com.bowyer.app.playermanage.databinding.ActivityPlayerSearchBinding;
 import com.bowyer.app.playermanage.gcm.GrowthPush;
 import com.bowyer.app.playermanage.logic.GrowthPushLogic;
 import com.bowyer.app.playermanage.logic.GrowthbeatLogic;
@@ -34,8 +27,6 @@ import com.bowyer.app.playermanage.logic.ReviewLogic;
 import com.bowyer.app.playermanage.ui.adapter.PlayerAdapter;
 import com.bowyer.app.playermanage.ui.dialog.RankSelectDialogFragment;
 import com.github.aakira.expandablelayout.ExpandableLayoutListenerAdapter;
-import com.github.aakira.expandablelayout.ExpandableRelativeLayout;
-import com.github.ksoichiro.android.observablescrollview.ObservableRecyclerView;
 import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
 import com.github.ksoichiro.android.observablescrollview.ScrollState;
 import com.squareup.sqlbrite.BriteDatabase;
@@ -48,29 +39,21 @@ public class MainActivity extends AppCompatActivity
 
   @Inject BriteDatabase mDb;
 
-  @Bind(R.id.toolbar) Toolbar mToolbar;
-  @Bind(R.id.recycler_view) ObservableRecyclerView mRecyclerView;
-  @Bind(R.id.expand_linear) ExpandableRelativeLayout mExpandableHeader;
-  @Bind(R.id.ic_up) ImageView mIcUp;
-  @Bind(R.id.search_query_text) EditText mSearchQueryText;
-  @Bind(R.id.search_query_delete) ImageButton mSearchWordDelete;
-  @Bind(R.id.fab) FloatingActionButton mFab;
-  @Bind(R.id.sex_group) RadioGroup mSexGroup;
-  @Bind(R.id.rank_text) TextView mRankText;
-
   private PlayerAdapter mAdapter;
   private Subscription mSubscription;
+  private ActivityPlayerSearchBinding binding = null;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_player_search);
+    binding = DataBindingUtil.setContentView(this, R.layout.activity_player_search);
     ButterKnife.bind(this);
     PlayerApplication.getComponent(getApplicationContext()).inject(this);
     GrowthPushLogic.initGrowthPushSetting(this, getIntent());
     launchWebIfHasGrowthPush();
+    init();
     initRecyclerView();
     initSearchQuery();
-    mSexGroup.setOnCheckedChangeListener((radioGroup, i) -> {
+    binding.header.sexGroup.setOnCheckedChangeListener((radioGroup, i) -> {
       doSearch();
     });
   }
@@ -100,6 +83,61 @@ public class MainActivity extends AppCompatActivity
     ButterKnife.unbind(this);
   }
 
+  private void init() {
+    binding.header.expandActionButton.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        binding.header.expandLinear.toggle();
+        binding.header.expandLinear.setListener(new ExpandableLayoutListenerAdapter() {
+          @Override public void onOpened() {
+            super.onOpened();
+            final Animator rotateUpAnim = ObjectAnimator.ofPropertyValuesHolder(binding.header.icUp,
+                PropertyValuesHolder.ofFloat(View.ROTATION, 0f, 180f));
+            rotateUpAnim.setDuration(250L);
+            rotateUpAnim.start();
+          }
+
+          @Override public void onClosed() {
+            super.onClosed();
+            final Animator rotateDownAnim =
+                ObjectAnimator.ofPropertyValuesHolder(binding.header.icUp,
+                    PropertyValuesHolder.ofFloat(View.ROTATION, 180f, 0f));
+            rotateDownAnim.setDuration(250L);
+            rotateDownAnim.start();
+          }
+        });
+      }
+    });
+    binding.searchQueryDelete.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        binding.searchQueryText.setText("");
+      }
+    });
+    binding.fab.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        startPlayerManage();
+      }
+    });
+    binding.header.rank.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        onClickRank();
+      }
+    });
+    binding.setting.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        onClickSetting();
+      }
+    });
+    binding.privacy.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        onClickPrivacy();
+      }
+    });
+  }
+
+  private void startPlayerManage() {
+    PlayerManageActivity.startActivity(this, null);
+  }
+
   private void launchWebIfHasGrowthPush() {
     Intent intent = getIntent();
     if (intent.hasExtra(GrowthPush.KEY_GROWTH_PUSH)) {
@@ -113,24 +151,24 @@ public class MainActivity extends AppCompatActivity
 
   private void initRecyclerView() {
     LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-    mRecyclerView.setLayoutManager(linearLayoutManager);
-    mRecyclerView.setHasFixedSize(true);
+    binding.recyclerView.setLayoutManager(linearLayoutManager);
+    binding.recyclerView.setHasFixedSize(true);
     mAdapter = new PlayerAdapter(this);
-    mRecyclerView.setAdapter(mAdapter);
-    mRecyclerView.setScrollViewCallbacks(this);
+    binding.recyclerView.setAdapter(mAdapter);
+    binding.recyclerView.setScrollViewCallbacks(this);
   }
 
   private void initSearchQuery() {
-    mSearchQueryText.addTextChangedListener(new TextWatcher() {
+    binding.searchQueryText.addTextChangedListener(new TextWatcher() {
       @Override public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
       }
 
       @Override public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
         if (TextUtils.isEmpty(charSequence)) {
-          mSearchWordDelete.setVisibility(View.INVISIBLE);
+          binding.searchQueryDelete.setVisibility(View.INVISIBLE);
         } else {
-          mSearchWordDelete.setVisibility(View.VISIBLE);
+          binding.searchQueryDelete.setVisibility(View.VISIBLE);
         }
         doSearch();
       }
@@ -141,39 +179,10 @@ public class MainActivity extends AppCompatActivity
     });
   }
 
-  @OnClick(R.id.expand_action_button) void headerTranslation() {
-    mExpandableHeader.toggle();
-    mExpandableHeader.setListener(new ExpandableLayoutListenerAdapter() {
-      @Override public void onOpened() {
-        super.onOpened();
-        final Animator rotateUpAnim = ObjectAnimator.ofPropertyValuesHolder(mIcUp,
-            PropertyValuesHolder.ofFloat(View.ROTATION, 0f, 180f));
-        rotateUpAnim.setDuration(250L);
-        rotateUpAnim.start();
-      }
-
-      @Override public void onClosed() {
-        super.onClosed();
-        final Animator rotateDownAnim = ObjectAnimator.ofPropertyValuesHolder(mIcUp,
-            PropertyValuesHolder.ofFloat(View.ROTATION, 180f, 0f));
-        rotateDownAnim.setDuration(250L);
-        rotateDownAnim.start();
-      }
-    });
-  }
-
-  @OnClick(R.id.search_query_text) void searchQueryTextClick() {
-    // do nothing
-  }
-
-  @OnClick(R.id.search_query_delete) void deleteSearchQuery() {
-    mSearchQueryText.setText("");
-  }
-
   private void doSearch() {
-    String name = mSearchQueryText.getText().toString();
-    Sex sex = getCheckedSex(mSexGroup.getCheckedRadioButtonId());
-    Rank rank = Rank.of(mRankText.getText().toString());
+    String name = binding.searchQueryText.getText().toString();
+    Sex sex = getCheckedSex(binding.header.sexGroup.getCheckedRadioButtonId());
+    Rank rank = Rank.of(binding.header.rankText.getText().toString());
     PlayerDao.PlayerQuery playerQuery =
         PlayerDao.SQLBuilder.with().name(name).sex(sex).rank(rank).build();
     mSubscription = PlayerDao.getPlayersByQuery(mDb, playerQuery)
@@ -201,37 +210,33 @@ public class MainActivity extends AppCompatActivity
   }
 
   @Override public void onUpOrCancelMotionEvent(ScrollState scrollState) {
-    if (mExpandableHeader.isExpanded()) {
-      mExpandableHeader.collapse();
+    if (binding.header.expandLinear.isExpanded()) {
+      binding.header.expandLinear.collapse();
     }
     if (scrollState == ScrollState.UP) {
-      mFab.hide();
+      binding.fab.hide();
     } else {
-      mFab.show();
+      binding.fab.show();
     }
   }
 
-  @OnClick(R.id.fab) void onClickAddPlayer() {
-    PlayerManageActivity.startActivity(this, null);
-  }
-
-  @OnClick(R.id.rank) void onClickRank() {
+  private void onClickRank() {
     RankSelectDialogFragment.newInstance().show(getSupportFragmentManager(), "");
   }
 
-  @OnClick(R.id.setting) void onClickSetting() {
+  private void onClickSetting() {
     LicenseActivity.startActivity(this);
   }
 
-  @OnClick(R.id.privacy) void onClickPrivacy() {
+  private void onClickPrivacy() {
     WebViewActivity.startWebViewActivity(this, "https://kyudo-bowyer.com/?p=656");
   }
 
   @Override public void onRankSelect(String rank) {
     if (!TextUtils.isEmpty(rank)) {
-      mRankText.setText(rank);
+      binding.header.rankText.setText(rank);
     } else {
-      mRankText.setText(getString(R.string.player_rank_all));
+      binding.header.rankText.setText(getString(R.string.player_rank_all));
     }
     doSearch();
   }
--- app/src/main/java/com/bowyer/app/playermanage/ui/dialog/GooglePlusDialogFragment.java
+++ app/src/main/java/com/bowyer/app/playermanage/ui/dialog/GooglePlusDialogFragment.java
@@ -1,26 +1,24 @@
 package com.bowyer.app.playermanage.ui.dialog;
 
-import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
-import butterknife.Bind;
-import butterknife.ButterKnife;
+import android.view.ViewGroup;
 import com.bowyer.app.playermanage.BuildConfig;
 import com.bowyer.app.playermanage.PlayerApplication;
 import com.bowyer.app.playermanage.R;
+import com.bowyer.app.playermanage.databinding.DialogGooglePlusBinding;
 import com.bowyer.app.playermanage.preference.ReviewPreferences;
-import com.google.android.gms.plus.PlusOneButton;
 
 public class GooglePlusDialogFragment extends DialogFragment {
 
   private static final String KEY_COUNT = "key_count";
-
-  @Bind(R.id.plus_one_button) PlusOneButton mPlusOneButton;
+  private DialogGooglePlusBinding binding = null;
 
   public static GooglePlusDialogFragment newInstance(int count) {
     GooglePlusDialogFragment f = new GooglePlusDialogFragment();
@@ -30,36 +28,35 @@ public class GooglePlusDialogFragment extends DialogFragment {
     return f;
   }
 
-  @Override public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    LayoutInflater layoutInflater =
-        (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    View content = layoutInflater.inflate(R.layout.dialog_google_plus, null);
-
-    PlayerApplication.getComponent(getContext()).inject(this);
-    ReviewPreferences preferences = new ReviewPreferences(getActivity());
-    int count = getArguments().getInt(KEY_COUNT, 0);
-    preferences.reviewShowed(count);
+  @Nullable @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    binding = DialogGooglePlusBinding.inflate(inflater, container, false);
 
     AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
     builder.setTitle(R.string.google_plus_dialog_title);
-    builder.setView(content);
+    builder.setView(binding.getRoot());
 
     builder.setPositiveButton(android.R.string.cancel, (dialogInterface, i) -> {
       dismiss();
     });
-
-    Dialog dialog = builder.create();
-    ButterKnife.bind(this, content);
     initGooglePlus();
     this.setCancelable(false);
-    return dialog;
+    return binding.getRoot();
+  }
+
+  @Override public void onAttach(Context context) {
+    PlayerApplication.getComponent(getContext()).inject(this);
+    ReviewPreferences preferences = new ReviewPreferences(getActivity());
+    int count = getArguments().getInt(KEY_COUNT, 0);
+    preferences.reviewShowed(count);
+    super.onAttach(context);
   }
 
   private void initGooglePlus() {
     String applicationId = BuildConfig.APPLICATION_ID.replace("." + BuildConfig.BUILD_TYPE, "");
     String url = "https://market.android.com/details?id=" + applicationId;
     int requestCode = 1;
-    mPlusOneButton.initialize(url, requestCode);
+    binding.plusOneButton.initialize(url, requestCode);
   }
 }
