94a79b0b23a86aacca7a24cb8525b7bf3adfc4a4
bowyer <bowyer.app@gmail.com>
2021-04-26 16:22:55 +0900

    remove multidex

--- app/src/main/java/com/bowyer/app/playermanage/PlayerApplication.java
+++ app/src/main/java/com/bowyer/app/playermanage/PlayerApplication.java
@@ -2,25 +2,21 @@ package com.bowyer.app.playermanage;
 
 import android.app.Application;
 import android.content.Context;
-import android.support.multidex.MultiDex;
+
 import com.bowyer.app.playermanage.logic.GrowthbeatLogic;
 
 public class PlayerApplication extends Application {
-  private PlayerAppComponent playerAppComponent;
+    private PlayerAppComponent playerAppComponent;
 
-  @Override public void onCreate() {
-    super.onCreate();
-    playerAppComponent = DaggerPlayerAppComponent.builder()
-        .playerModule(new PlayerModule(this)).build();
-    GrowthbeatLogic.init(this);
-  }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        playerAppComponent = DaggerPlayerAppComponent.builder()
+                .playerModule(new PlayerModule(this)).build();
+        GrowthbeatLogic.init(this);
+    }
 
-  public static PlayerAppComponent getComponent(Context context) {
-    return ((PlayerApplication) context.getApplicationContext()).playerAppComponent;
-  }
-
-  @Override protected void attachBaseContext(Context base) {
-    super.attachBaseContext(base);
-    MultiDex.install(this);
-  }
+    public static PlayerAppComponent getComponent(Context context) {
+        return ((PlayerApplication) context.getApplicationContext()).playerAppComponent;
+    }
 }
